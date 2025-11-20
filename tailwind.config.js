0579a01fc6664ee5aeaaf8b967807a1c9e13437e
notsonerd9 <04patricia22@gmail.com>
2025-11-20 11:28:31 +0000

    Compilación final de CSS y limpieza de secretos/binarios.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html", // Indica a Tailwind que escanee todos los archivos HTML en templates/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
